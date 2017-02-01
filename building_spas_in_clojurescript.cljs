930d57cc8e42267c7d923e9deec6d3274aefffc7
bashhack <info@marclaughton.com>
2017-01-31 21:18:55 -0600

    Checking in initial React project

---
+++ building_spas_in_clojurescript.cljs
@@ -0,0 +1,32 @@
+;; Building Single Page Applications
+;; =================================
+
+
+;; Here, we'll get into the nitty gritty of building
+;; a Single Page Application (SPA) using Om and React.js
+;; In total, we'll cover:
+;; - Om and React.js
+;; - Routing using bidi
+;; - HTML5 history and pushState
+;; - REST APIs using AJAX
+
+
+;; --------------------------------------
+;; --------------------------------------
+;; Understanding Single Page Applications
+;; --------------------------------------
+;; --------------------------------------
+
+
+;; Advantages:
+;; - They provide a richer UI
+;; - Easier to deal with client-side state and data
+;; - Easier to deal with AJAX
+;; - Faster client interactions once the page is loaded
+
+;; Disadvantages:
+;; - More development work
+;; - Initial page load is usually slower
+;; - Legacy browser support is harder
+
+;; For this deep dive, our project will live in the `om-tut` directory
